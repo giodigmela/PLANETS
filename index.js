1a615fdc82858c2468a85a7d0be9c5a76c388fab
giorgi20016 <87188095+HotelGareji@users.noreply.github.com>
2022-03-20 12:01:50 +0400

    click function

--- index.js
+++ index.js
@@ -9,15 +9,49 @@ setInterval(function(){
     }
 }, 5000);
 
-
-var TSS = document.getElementById("TSS")
-setTimeout( function() { TSS.className = 'after'}, 7000 );
-
-
 var enter = document.getElementById("enter")
 
-enter.addEventListener("click", function(){setTimeout( function() { window.location = 'index_two.html'}, 0 );}, false);
+enter.addEventListener("click", function(){
+        
+        setTimeout( function() { window.location = 'index_two.html'}, 42000 );
 
+        setTimeout( function() { document.getElementById("enter_1").className = "none"; }, 500 );
+        setTimeout( function() { document.getElementById("enter_2").className = "none"; }, 500 );
+
+        document.getElementById("blacked").className = "zero"; 
+
+        document.getElementById("uranus").className = "one";
+        document.getElementById("t_1").className = "t_one";
+
+        document.getElementById("neptune").className = "two";
+        document.getElementById("t_2").className = "t_two";
+
+        document.getElementById("venus").className = "three";
+        document.getElementById("t_3").className = "t_three";
+
+        document.getElementById("mars").className = "four";
+        document.getElementById("t_4").className = "t_four";
+
+        document.getElementById("mercury").className = "five";
+        document.getElementById("t_5").className = "t_five";
+
+        document.getElementById("saturn").className = "six";
+        document.getElementById("t_6").className = "t_six";
+
+        document.getElementById("saturn_bg").className = "six_half";
+
+        document.getElementById("jupiter").className = "seven";
+        document.getElementById("t_7").className = "t_seven";
+
+        document.getElementById("earth").className = "eight";
+        document.getElementById("t_8").className = "t_eight";
+
+        document.getElementById("sun").className = "nine";
+        document.getElementById("t_9").className = "t_nine";
+
+        setTimeout( function() { document.getElementById("blacked_two").className = "ten"; }, 41000 );
+
+    }, false);
 
 setTimeout( function() { window.addEventListener("keypress", checkKeyPress, false)}, 17000 );
 function checkKeyPress(key){
@@ -62,3 +96,6 @@ function checkKeyPress(key){
 
     }
 }
+
+var TSS = document.getElementById("TSS")
+setTimeout( function() { TSS.className = 'after'}, 7000 );
